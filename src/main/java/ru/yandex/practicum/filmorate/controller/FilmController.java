304c36414a1529eed47888d8b065deb118cc2ed3
KatyaKosnova <kosnovakaty@gmail.com>
2024-12-07 22:18:34 +0500

    доработка

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -8,6 +8,7 @@ import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 import java.util.List;
 
@@ -22,42 +23,48 @@ public class FilmController {
         this.filmService = filmService;
     }
 
-    // Получение фильма по ID
     @GetMapping("/{id}")
     public Film getFilmById(@PathVariable @Positive int id) {
         log.info("Получение фильма с ID: {}", id);
         return filmService.getFilmById(id);
     }
 
-    // Добавление лайка фильму
+    @PostMapping
+    public Film createFilm(@RequestBody @Valid Film film) {
+        log.info("Создание нового фильма: {}", film);
+        return filmService.addFilm(film);
+    }
+
+    @PutMapping
+    public Film updateFilm(@RequestBody @Valid Film film) {
+        log.info("Обновление фильма: {}", film);
+        return filmService.updateFilm(film);
+    }
+
     @PutMapping("/{id}/like/{userId}")
     public Film addLike(@PathVariable @Positive int id, @PathVariable @Positive int userId) {
         log.info("Добавление лайка для фильма с ID: {} от пользователя с ID: {}", id, userId);
         return filmService.addLike(id, userId);
     }
 
-    // Удаление лайка у фильма
     @DeleteMapping("/{id}/like/{userId}")
     public Film removeLike(@PathVariable @Positive int id, @PathVariable @Positive int userId) {
         log.info("Удаление лайка для фильма с ID: {} от пользователя с ID: {}", id, userId);
         return filmService.removeLike(id, userId);
     }
 
-    // Получение списка популярных фильмов
     @GetMapping("/popular")
-    public List<Film> getPopularFilms(@RequestParam(defaultValue = "10") int count) {
+    public List<Film> getPopularFilms(@RequestParam(defaultValue = "10") @Positive int count) {
         log.info("Получение топ {} популярных фильмов", count);
         return filmService.getMostPopularFilms(count);
     }
 
-    // Получение всех фильмов
     @GetMapping
     public List<Film> getAllFilms() {
         log.info("Получение всех фильмов");
         return filmService.getAllFilms();
     }
 
-    // Удаление фильма
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteFilm(@PathVariable @Positive int id) {
         filmService.deleteFilm(id);
@@ -65,10 +72,15 @@ public class FilmController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Фильм удален");
     }
 
-    // Обработка исключений
     @ExceptionHandler(FilmNotFoundException.class)
     public ResponseEntity<String> handleFilmNotFoundException(FilmNotFoundException ex) {
         log.error("Фильм не найден: {}", ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        log.error("Ошибка: {}", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Произошла ошибка на сервере");
     }
 }
