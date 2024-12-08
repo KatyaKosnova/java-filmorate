a54c255efa9301214900449208840825926944fd
KatyaKosnova <kosnovakaty@gmail.com>
2024-12-08 19:50:37 +0500

    доработка

--- src/main/java/ru/yandex/practicum/filmorate/model/JacksonConfig.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/JacksonConfig.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.model;
 
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
